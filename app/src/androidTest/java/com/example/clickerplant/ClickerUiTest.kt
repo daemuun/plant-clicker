9d4da102c1f08bcf95691c58b1dfb160ee867337
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-09-04 19:50:16 +0300

    fix: isolate real repository

--- app/src/androidTest/java/com/example/clickerplant/ClickerUiTest.kt
+++ app/src/androidTest/java/com/example/clickerplant/ClickerUiTest.kt
@@ -1,10 +1,13 @@
 package com.example.clickerplant
 
+import android.annotation.SuppressLint
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import com.example.clickerplant.data.MockPlantsRepository
 import com.example.clickerplant.ui.ClickerScreen
+import com.example.clickerplant.ui.ClickerViewModel
 import com.example.clickerplant.ui.theme.ClickerPlantTheme
 import org.junit.Rule
 import org.junit.Test
@@ -13,11 +16,12 @@ class ClickerUiTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @SuppressLint("ViewModelConstructorInComposable")
     @Test
     fun on_user_click_count_clicks_increased() {
         composeTestRule.setContent {
             ClickerPlantTheme {
-                ClickerScreen()
+                ClickerScreen(ClickerViewModel(MockPlantsRepository()))
             }
         }
 
@@ -25,11 +29,12 @@ class ClickerUiTest {
         composeTestRule.onNodeWithText("Количество кликов: 1").assertExists()
     }
 
+    @SuppressLint("ViewModelConstructorInComposable")
     @Test
     fun on_user_click_updated_revenue() {
         composeTestRule.setContent {
             ClickerPlantTheme {
-                ClickerScreen()
+                ClickerScreen(ClickerViewModel(MockPlantsRepository()))
             }
         }
 
