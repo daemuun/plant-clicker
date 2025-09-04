097d1c5441017d977657942f446373ab8d1bd07e
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-09-04 19:17:11 +0300

    feat: update plants repository

--- app/src/main/java/com/example/clickerplant/data/PlantsRepoitoryImpl.kt
+++ app/src/main/java/com/example/clickerplant/data/PlantsRepoitoryImpl.kt
@@ -7,7 +7,11 @@ class PlantsRepositoryImpl : PlantsRepository {
     override fun getPlants(): List<Plant> {
         return listOf(
             Plant(R.drawable.cucumber, R.string.cucumber, 20, 20),
-            Plant(R.drawable.tomato, R.string.tomato, 40, 40)
+            Plant(R.drawable.tomato, R.string.tomato, 40, 40),
+            Plant(R.drawable.garlic, R.string.garlic, 200, 150),
+            Plant(R.drawable.chili, R.string.chili, 500, 240),
+            Plant(R.drawable.potato, R.string.potato, 1200, 500),
+            Plant(R.drawable.onion, R.string.onion, 2000, 1000),
         ).sortedBy { it.countThisPlantTaps }
     }
 }
